fbccecf061d7369479cbb854c2a05c910ea6146c
bradley <bradleyisbackagain@gmail.com>
2022-04-11 14:07:29 +0100

    Foo: Remove storyboard, instantiate ViewController as root
    We will not use storyboards, constructing views and autolayout
    contraints in code only.

--- Directory/ViewController.swift
+++ Directory/ViewController.swift
@@ -12,8 +12,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        
+        let label = UILabel()
+        label.text = "Hello world"
+        view.addSubview(label)
+        view.edgeConstrain(subview: label, layoutGuide: .safeArea)
     }
-
 
 }
 
