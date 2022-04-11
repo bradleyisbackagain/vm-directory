a65496309532ee811ce5a3f8fb71b3c1e3cb2e0a
bradley <bradleyisbackagain@gmail.com>
2022-04-11 14:07:32 +0100

    DirectoryService: add decoding tests for Person and Room

---
+++ Packages/DirectoryService/Tests/DirectoryServiceTests/PersonTests.swift
@@ -0,0 +1,52 @@
+//
+//  PersonTests.swift
+//
+//  Created by Bradley Mackey on 11/04/2022.
+//
+
+import XCTest
+import CommonUtils
+@testable import DirectoryService
+
+class PersonTests: XCTestCase {
+    
+    var decoder: JSONDecoder!
+
+    override func setUpWithError() throws {
+        decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .custom(ISO8601MillisecondsDecoder.decode)
+    }
+
+    override func tearDownWithError() throws {
+    }
+    
+    func test_decodesFromJSON() throws {
+        let string = """
+        {
+          "createdAt": "2022-01-24T17:02:23.729Z",
+          "firstName": "Maggie",
+          "avatar": "https://randomuser.me/api/portraits/women/21.jpg",
+          "lastName": "Brekke",
+          "email": "Crystel.Nicolas61@hotmail.com",
+          "jobtitle": "Future Functionality Strategist",
+          "favouriteColor": "pink",
+          "id": "1"
+        }
+        """
+        let data = string.data(using: .utf8)!
+        
+        let decoded = try decoder.decode(Person.self, from: data)
+        
+        XCTAssertEqual(decoded.firstName, "Maggie")
+        XCTAssertEqual(decoded.lastName, "Brekke")
+        XCTAssertEqual(decoded.email, "Crystel.Nicolas61@hotmail.com")
+        XCTAssertEqual(decoded.jobTitle, "Future Functionality Strategist")
+        XCTAssertEqual(decoded.favouriteColor, "pink")
+        XCTAssertEqual(decoded.id, "1")
+        XCTAssertEqual(decoded.avatar.absoluteString, "https://randomuser.me/api/portraits/women/21.jpg")
+        
+        let expectedDate = Date(timeIntervalSince1970: 1643043743.729)
+        XCTAssertEqual(decoded.createdAt, expectedDate)
+    }
+
+}
---
+++ Packages/DirectoryService/Tests/DirectoryServiceTests/RoomTests.swift
@@ -0,0 +1,44 @@
+//
+//  RoomTests.swift
+//
+//  Created by Bradley Mackey on 11/04/2022.
+//
+
+import XCTest
+import CommonUtils
+@testable import DirectoryService
+
+class RoomTests: XCTestCase {
+    
+    var decoder: JSONDecoder!
+
+    override func setUpWithError() throws {
+        decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .custom(ISO8601MillisecondsDecoder.decode)
+    }
+
+    override func tearDownWithError() throws {
+    }
+    
+    func test_decodesFromJSON() throws {
+        let string = """
+        {
+        "createdAt": "2022-01-24T17:02:23.729Z",
+        "isOccupied": false,
+        "maxOccupancy": 53539,
+        "id": "1"
+        }
+        """
+        let data = string.data(using: .utf8)!
+        
+        let decoded = try decoder.decode(Room.self, from: data)
+        
+        XCTAssertEqual(decoded.isOccupied, false)
+        XCTAssertEqual(decoded.maxOccupancy, 53539)
+        XCTAssertEqual(decoded.id, "1")
+        
+        let expectedDate = Date(timeIntervalSince1970: 1643043743.729)
+        XCTAssertEqual(decoded.createdAt, expectedDate)
+    }
+
+}
