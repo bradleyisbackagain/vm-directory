d996987502e8aba4f5691f915126b287b247d5de
bradley <bradleyisbackagain@gmail.com>
2022-04-11 14:07:31 +0100

    DetailItem: add DetailItemTableViewController
    This is for showing the detail of a given item.

---
+++ Directory/Features/DetailItem/DetailItemTableViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DetailItemTableViewController.swift
+//  Directory
+//
+//  Created by Bradley Mackey on 11/04/2022.
+//
+
+import UIKit
+
+/// Displays a list of detail items.
+class DetailItemTableViewController: UITableViewController {
+    var items = [DetailItemViewModel]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let item = items[indexPath.row]
+        let id = "detail-cell"
+            let cell = tableView.dequeueReusableCell(withIdentifier: id) ?? UITableViewCell(style: .value1, reuseIdentifier: id)
+        cell.textLabel?.text = item.title
+        cell.detailTextLabel?.text = item.detail
+        return cell
+    }
+}
---
+++ Directory/Features/DetailItem/DetailItemViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  DetailItemViewModel.swift
+//  Directory
+//
+//  Created by Bradley Mackey on 11/04/2022.
+//
+
+import Foundation
+
+struct DetailItemViewModel {
+    var title: String
+    var detail: String
+}
