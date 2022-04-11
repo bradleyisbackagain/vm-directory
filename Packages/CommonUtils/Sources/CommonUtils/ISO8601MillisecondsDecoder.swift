b035e0af56585d63988cf9cd7a7c0c4cf5d36c89
bradley <bradleyisbackagain@gmail.com>
2022-04-11 14:07:32 +0100

    CommonUtils: add ISO8601MillisecondsDecoder
    This is because JSONDecoder requires a custom strategy to decode
    milliseconds in ISO8601.

---
+++ Packages/CommonUtils/Sources/CommonUtils/ISO8601MillisecondsDecoder.swift
@@ -0,0 +1,39 @@
+//
+//  ISO8601MillisecondsDecoder.swift
+//
+//  Created by Bradley Mackey on 11/04/2022.
+//
+
+import Foundation
+
+/// Decoding context for ISO8601 with milliseconds.
+///
+/// This is not supported by default by `JSONDecoder`, so we define a custom strategy here.
+/// See: https://stackoverflow.com/a/55470440
+public enum ISO8601MillisecondsDecoder {
+    private static let formatter: ISO8601DateFormatter = {
+        let formatter = ISO8601DateFormatter()
+        formatter.formatOptions = [
+            .withFullDate,
+            .withFullTime,
+            .withTimeZone,
+            .withFractionalSeconds,
+        ]
+        return formatter
+    }()
+    
+    /// Using the decoder, decode an ISO8601 date string (with milliseconds).
+    public static func decode(_ decoder: Decoder) throws -> Date {
+        let container = try decoder.singleValueContainer()
+        let string = try container.decode(String.self)
+        if let date = formatter.date(from: string) {
+            return date
+        } else {
+            let context = DecodingError.Context(
+                codingPath: decoder.codingPath,
+                debugDescription: "The data is not ISO8601 formatted with milliseconds!"
+            )
+            throw DecodingError.dataCorrupted(context)
+        }
+    }
+}
---
+++ Packages/CommonUtils/Tests/CommonUtilsTests/ISO8601MillisecondsDecoderTests.swift
@@ -0,0 +1,39 @@
+//
+//  ISO8601MillisecondsDecoderTests.swift
+//
+//  Created by Bradley Mackey on 11/04/2022.
+//
+
+import XCTest
+@testable import CommonUtils
+
+class ISO8601MillisecondsDecoderTests: XCTestCase {
+    
+    private struct DemoContainer: Decodable {
+        var d: Date
+    }
+    
+    var decoder: JSONDecoder!
+   
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        
+        decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .custom(ISO8601MillisecondsDecoder.decode)
+    }
+    
+    func test_decodeWithMilliseconds() throws {
+        let string = #"{"d":"2022-01-24T17:02:23.729Z"}"#
+        let data = string.data(using: .utf8)!
+        let decoded = try decoder.decode(DemoContainer.self, from: data)
+        
+        let expected = Date(timeIntervalSince1970: 1643043743.729)
+        XCTAssertEqual(expected, decoded.d)
+    }
+    
+    func test_cannotDecodeWithoutMilliseconds() throws {
+        let string = #"{"d":"2022-01-24T17:02:23Z"}"#
+        let data = string.data(using: .utf8)!
+        XCTAssertThrowsError(try decoder.decode(DemoContainer.self, from: data))
+    }
+}
